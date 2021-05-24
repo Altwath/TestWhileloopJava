7ac2b06d2940592c154abe4cb8d58bcf7669ca27
First Last <haidar.altwat@gmail.com>
2021-05-24 11:06:29 +0000

    commit

---
+++ TestWhileLoop.java
@@ -0,0 +1,13 @@
+Package com.simplelearn.loop;
+public class TestWhileLoop { 
+ public static void main(String[] args){
+ //simple counter example 
+int count =0;
+while(count<10){
+System.out.println("The count value :: "+ count );
+//terminator
+count ++;
+
+}
+}
+}
